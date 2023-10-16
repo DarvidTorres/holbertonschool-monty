c246bfc10ba9f36d81c53075a9297ad701d60718
Darvid <darvid.torres@gmail.com>
2023-10-15 19:59:55 -0600

    handle functions

--- monty.h
+++ monty.h
@@ -26,11 +26,12 @@ char *buff;
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO
  */
+
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -43,8 +44,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int);
 } instruction_t;
 
 /* PROTOTYPES */
