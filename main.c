b64bd2694d043a1a6a63a216593770cb5b98dc98
Darvid <darvid.torres@gmail.com>
2023-10-17 12:53:20 -0600

    main function concise

--- main.c
+++ main.c
@@ -6,77 +6,40 @@
  * @argv: Argument values
  * Return: 0 on success, 1 on failure
  */
-int main(int argc, char *argv[])
-{
-	char *opcode, *value, *line = NULL;
+int main(int argc, char *argv[]) {
+	if (argc != 2) error_usage();
+	FILE *file = fopen(argv[1], "r");
+	if (!file) error_file_open(argv[1]);
+
+	char *line = NULL, *opcode, *value;
 	size_t line_number = 0, len = 0;
-	FILE *file;
-	int i, j, flag;
 	stack_t *stack = NULL;
 
-	if (argc != 2)
-	{
-		error_usage();
-	}
-	file = fopen(argv[1], "r");
-	if (file == NULL)
-	{
-		error_file_open(argv[1]);
-	}
-	instruction_t opcodes[] = {
-		{"pall", pall},
-		{"pint", pint},
-		{"pop", pop},
-		{"swap", swap},
-		{"add", add},
-		{"nop", nop},
-		{NULL, NULL},
-	};
-	while (getline(&line, &len, file) != -1)
-	{
-		flag = 0;
+	instruction_t opcodes[] = {{"pall", pall}, {"pint", pint}, {"pop", pop}, {"swap", swap}, {"add", add}, {"nop", nop}, {NULL, NULL}};
+
+	while (getline(&line, &len, file) != -1) {
 		line_number++;
 		opcode = strtok(line, " \t$\n");
-		if (opcode == NULL || opcode[0] == '#')
-			continue;
-		if (strcmp(opcode, "push") == 0)
-		{
+		if (!opcode || opcode[0] == '#') continue;
+		if (strcmp(opcode, "push") == 0) {
 			value = strtok(NULL, " $\n");
-			if (value == NULL)
-			{
-				print_push_error(line_number, file, line);
-			}
-			for (i = 0; value[i] != '\0'; i++)
-			{
-				if  (!isdigit(value[i]) && value[i] != '-')
-				{
-					print_push_error(line_number, file, line);
-				}
-			}
-		push(&stack, atoi(value));
-		flag = 1;
+			if (!value || (value[0] != '-' && !isdigit(value[0]))) print_push_error(line_number, file, line);
+			push(&stack, atoi(value));
+		} else {
+			int j;
+			for (j = 0; opcodes[j].opcode && strcmp(opcode, opcodes[j].opcode) != 0; j++);
+			if (!opcodes[j].opcode) invalid_inst(line_number, opcode, file, line, stack);
+			else opcodes[j].f(&stack, line_number);
 		}
-		if (flag == 0)
-		{
-			for (j = 0; opcodes[j].opcode != NULL; j++)
-			{
-				if (strcmp(opcode, opcodes[j].opcode) == 0)
-				{
-					opcodes[j].f(&stack, line_number);
-					break;
-				}
-			}
-		}
-		if (j == 6)
-			invalid_inst(line_number, opcode, file, line, stack);
-	free(line);
-	line = NULL;
+		free(line);
+		line = NULL;
 	}
 	free_dlistint(stack);
 	free(line);
 	fclose(file);
-	return (0);
+	return 0;
 }
+
 
 /**
  * push - Pushes an element onto the stack
